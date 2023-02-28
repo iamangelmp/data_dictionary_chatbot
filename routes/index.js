0acee9eb288ca16a475a0099a4af239089b0cf25
Angel Marquez <amarquez@e-bitware.com.mx>
2023-02-27 18:24:08 -0600

    endpoint to read message

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,13 @@
 var express = require('express');
 var router = express.Router();
+const checkMessage = require('../controllers/dictionary.controller.js')
 require('dotenv').config()
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
+
+router.post('/message',checkMessage )
 
 module.exports = router;
