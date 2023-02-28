a84b5ef939ae14d62b4a2b01a0a354e0f94bb966
Angel Marquez <amarquez@e-bitware.com.mx>
2023-02-27 18:22:18 -0600

    data example

---
+++ data/keywords.js
@@ -0,0 +1,47 @@
+const keywords = [
+  {
+    id: 1,
+    question: '¿Cómo puedo realizar el pago de mi póliza?',
+    answare: 'Para realizar tu pago escribe la palabra *pagar* para que pueda ayudarte. O por la transacción 6787 en una agencia de *Banco Industrial*.',
+    keywords: ['pago',
+      'pagos',
+      'poder',
+      'medio',
+      'formas',
+      'forma',
+      'realizar',
+      'pagar',
+      'cómo puedo realizar el pago de mi póliza',
+      'como puedo realizar el pago de mi poliza',
+      'pago de mi poliza',
+      'pago de mi póliza',
+      'pagar póliza',
+      'pagar poliza',
+      'pago de póliza',
+      'pago de poliza',
+      'pago pendiente',
+      'pago pendiente de póliza',
+      'pago pendiente de poliza'],
+    comments: 'Para realizar tu pago presiona el boton "Pagar".'
+  },
+  {
+    id: 2,
+    question: '¿Cuánto tiempo tiene validez mi autorización?',
+    answare: 'Tu autorización es válida durante 30 días calendario (incluye fines de semana y días de asueto), a partir de su fecha de emisión; y está sujeta a condiciones vigentes al momento del servicio.',
+    keywords: ['tiempo',
+      'valido',
+      'validez',
+      'valides',
+      'valida',
+      'cuánto tiempo tiene validez mi autorización',
+      'cuanto tiepo tiene validez mi autorizacion',
+      'tiempo de validez',
+      'tiempo de valides',
+      'validez de autorización',
+      'validez de autorizacion',
+      'tiempo de validez de autorización',
+      'tiempo de validez e autorizacion'],
+    comments: 'ok'
+  }]
+
+module.exports = keywords;
